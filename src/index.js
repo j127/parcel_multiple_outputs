ce7a28f396cb2b5dee7112dc30a05105cdb08f79
J. Cohen <hostelmarketing@gmail.com>
2020-06-04 00:14:52 -0700

    Draw the rest of the owl

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { whatever } from "./somethingElse";
+
+console.log("this is index.js");
+
+console.log(whatever(7))
---
+++ src/somethingElse.js
@@ -0,0 +1,4 @@
+export function whatever(x) {
+    console.log("you passed in", x);
+    return 2 ** x;
+}
